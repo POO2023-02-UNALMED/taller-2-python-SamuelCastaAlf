76982284e73539f149f88c3781e664322c57f1ad
SamuelCastaAlf <92555846+SamuelCastaAlf@users.noreply.github.com>
2023-08-22 00:58:42 -0500

    Correcciones?
    Otra prueba

--- main.py
+++ main.py
@@ -25,9 +25,9 @@ class Auto:
     def __init__(self,modelo,precio,asientos,marca,motor,registro):
         self.modelo = modelo
         self.precio= precio
-        self.asientos = []
+        self.asientos = asientos
         self.marca = marca
-        self.motor = Motor
+        self.motor = motor
         self.registro = registro
     def cantidadAsientos(self):
         countAsientos=0
